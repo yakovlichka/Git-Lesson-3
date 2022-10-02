7c129ae75bc8d942619cfd83a5fa55b410775b34
Ekaterina <iakovleva_1990icloud.com>
2022-10-02 16:33:35 +0600

    Домашнее задание

--- C#/Homework2.cs
+++ C#/Homework2.cs
@@ -2,33 +2,3 @@
 //2, 3, 7 -> 7
 //44 5 78 -> 78
 //22 3 9 -> 22
-
-Console.WriteLine("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите третье число: ");
-int thirdNumber = Convert.ToInt32(Console.ReadLine());
-
-if (firstNumber > secondNumber)
-{
-    if (firstNumber > thirdNumber)
-    {
-        Console.WriteLine("Максимальное число: " + firstNumber);
-    }
-    else
-    {
-        Console.WriteLine("Максимальное число: " + thirdNumber);
-    }
-}
-
-else if (secondNumber > thirdNumber)
-{
-    Console.WriteLine("Максимальное число: " + secondNumber);
-}
-else
-{
-    Console.WriteLine("Максимальное число: " + thirdNumber);
-}
